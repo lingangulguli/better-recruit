279a8f65cc59f904f87d691f1dabafaeb0a6c041
Aaron Wen <siyuanwe@g.ucla.edu>
2026-04-20 00:43:39 -0700

    Delete recruit-poster directory

--- recruit-poster/packages/miniprogram/miniprogram/app.js
+++
@@ -1,8 +0,0 @@
-App({
-  globalData: {
-    sceneId: null,
-    answers: {},
-    generated: null,
-    selectedTemplate: null,
-  },
-});
--- recruit-poster/packages/shared/src/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './types';
-export * from './scenes';
-export * from './flows';
-export * from './templates';
-export * from './mockAi';
